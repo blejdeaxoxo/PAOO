0f2497b7559fd456e08b53763ef177034efc16bb
blejdeaxoxo <blejdeaemilalexandru@gmail.com>
2025-11-05 20:20:38 +0200

    added Task class

--- build/CMakeFiles/TaskManagerApp.dir/src/main.cpp.o.d
+++ build/CMakeFiles/TaskManagerApp.dir/src/main.cpp.o.d
@@ -148,4 +148,5 @@ CMakeFiles/TaskManagerApp.dir/src/main.cpp.o: \
  /usr/include/c++/13/bits/locale_facets.tcc \
  /usr/include/c++/13/bits/basic_ios.tcc \
  /usr/include/c++/13/bits/ostream.tcc /usr/include/c++/13/istream \
- /usr/include/c++/13/bits/istream.tcc
+ /usr/include/c++/13/bits/istream.tcc /home/alex/PAOO/src/Task.hpp \
+ /usr/include/c++/13/cstring /usr/include/string.h /usr/include/strings.h
